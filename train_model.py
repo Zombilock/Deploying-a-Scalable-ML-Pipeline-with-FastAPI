352d290235efe2899f33fb5ec5168e35287cacee
Christopher Clark <christopherjamesclark34@gmail.com>
2026-06-21 12:23:07 -0400

    Changed label to occupation

--- train_model.py
+++ train_model.py
@@ -77,7 +77,7 @@ for col in cat_features:
             column_name=col,
             slice_value=slicevalue,
             categorical_features=cat_features,
-            label="workclass",
+            label="occupation",
             encoder=encoder,
             lb=lb,
             model=model
