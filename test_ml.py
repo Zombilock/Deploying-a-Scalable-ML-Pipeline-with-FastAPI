b12559bb7190a76e4d719e6d2b048ebce6757c1d
Christopher Clark <christopherjamesclark34@gmail.com>
2026-06-21 13:22:12 -0400

    Fixed the first and second tests

--- test_ml.py
+++ test_ml.py
@@ -1,15 +1,19 @@
 import pytest
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-import ml.model as model
-from ml.model import train_model
+from ml.model import train_model, compute_model_metrics
 import os
+
+from train_model import X_train
 
 # TODO: implement the first test. Change the function name and input as needed
 def data_shape_test():
     """
     # Ensures that the number of rows is correct after processing the data
     """
-    assert data.shape[0] == '15', f'Expected 15 rows, but got {data.shape[0]}'
+    data = X_train.shape[0]
+
+    assert data == 15, f'Expected 15 rows, but got {data}'
     pass
 
 
@@ -18,6 +22,11 @@ def correct_model_test():
     """
     # Ensures that the model used is RandomForest
     """
+    X = np.array([[0, 0], [1, 1]])
+    y = np.array([0, 1])
+
+    model = train_model(X, y)
+
     assert isinstance(model, RandomForestClassifier), f'Expected model to be an instance of RandomForestClassifier, but got {type(model)}'
     pass
 
@@ -27,6 +36,10 @@ def results_test():
     """
     # Ensures that the model produces the expected results on the test set
     """
+    y_true = [0, 1, 0, 1, 0]
+    y_preds = [0, 1, 0, 0, 0]
+    precision, recall, fbeta = compute_model_metrics(y_true, y_preds)
+
     assert precision is not None, 'Expected precision to be calculated, but got None'
     assert recall is not None, 'Expected recall to be calculated, but got None'
     assert fbeta is not None, 'Expected fbeta to be calculated, but got None'
